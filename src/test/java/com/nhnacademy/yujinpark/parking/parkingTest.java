be906fcefe45840460d40694bb5c1ba93ad1a2e1
PYJ <yujin353@naver.com>
2022-04-08 10:27:03 +0900

    maven project skeleton

---
+++ src/test/java/com/nhnacademy/yujinpark/parking/parkingTest.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.yujinpark.parking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class parkingTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @Test
+    void test(){
+
+    }
+}
